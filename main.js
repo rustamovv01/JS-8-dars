579cf9e08d48c24db0e5c94396a854ca7352548c
Mukhammadaziz <rustamov977075073@gmail.com>
2024-03-12 11:50:21 +0500

    8-dars

---
+++ main.js
@@ -0,0 +1,32 @@
+// let a = ['Muhammadaziz', 'Fozil', 'Azim','Bosit'];
+// let b = confirm(a);
+
+// if (b === true) {
+//     a.shift('Bosit');
+//     console.log(a);
+// } else {
+//     console.log(a);
+// }
+
+// let a = ['olma', 'uzum', 'nok'];
+// let b = a.toString();
+// let res = b.toUpperCase();
+// console.log(res);
+
+// let a = ['olma', 'uzum', 'nok'];
+
+// for (i = 0; i = a.length; i++) {
+//     a[i] = a[i].toUpperCase();
+// }
+// console.log(a);
+
+let arr = [2, 4, 9, 10, 45, 67, 8, 90];
+let arr2 = [];
+
+for (i = 0; i = arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+        arr2.push(arr[i]);
+    }
+}
+
+console.log(arr2);
